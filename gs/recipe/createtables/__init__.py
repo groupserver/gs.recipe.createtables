196d92b6c22b981f946a4c53aec45878e88c0523
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-22 10:40:22 -0700

    Cleaning up the setup.py
    --HG--
    rename : docs/CHANGES.txt => docs/HISTORY.txt

--- gs/recipe/createtables/__init__.py
+++ gs/recipe/createtables/__init__.py
@@ -1,1 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+#lint:disable
+from .recipe import CreateTablesRecipe
+#lint:enable
--- setup.py
+++ setup.py
@@ -1,49 +1,59 @@
 # -*- coding: utf-8 -*-
-"""
-This module contains the tool of gs.recipe.createtables
-"""
+##############################################################################
+#
+# Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
-long_description = (
-    file('README.txt').read()
-    + '\n' +
-    file(os.path.join('docs', 'CONTRIBUTORS.txt')).read()
-    + '\n' +
-    file(os.path.join('docs', 'CHANGES.txt')).read()
-    + '\n'
-    )
-entry_point = 'gs.recipe.createtables:Recipe'
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
+entry_point = 'gs.recipe.createtables.recipe:CreateTablesRecipe'
 entry_points = {"zc.buildout": ["default = %s" % entry_point]}
 
 setup(name='gs.recipe.createtables',
       version=version,
-      description="Setup GroupServer instance in Zope",
+      description="Setup the GroupServer SQL tables in PostgreSQL",
       long_description=long_description,
       classifiers=[
+        "Development Status :: 4 - Beta",
         'Framework :: Buildout',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: Zope Public License',
-        "Development Status :: 4 - Beta",
-        "License :: Other/Proprietary License",
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        ],
-      keywords='zope groupserver recipe setup instance database',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+      ],
+      keywords='groupserver, recipe, setup, database, table',
       author='Michael JasonSmith',
       author_email='mpj17@onlinegroups.net',
       url='',
-      license='ZPL',
+      license='ZPL 2.1',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['gs', 'gs.recipe'],
       include_package_data=True,
-      zip_safe=True,
+      zip_safe=False,
       install_requires=[
         'setuptools',
         'zc.buildout',
