cd767add41fbbbf71e9e4e93cd893b1730de16f8
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-23 08:47:31 -0700

    Test the pipe

--- gs/recipe/createtables/tests/setupdb.py
+++ gs/recipe/createtables/tests/setupdb.py
@@ -14,7 +14,7 @@
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
 #import codecs
-#from mock import MagicMock, patch
+from mock import patch, MagicMock
 #import os
 #from shutil import rmtree
 #from tempfile import mkdtemp
@@ -26,14 +26,31 @@ UTF8 = 'utf-8'
 
 class TestSetupDB(TestCase):
 
-    def test_get_sql_filenames_from_products(self):
+    def test_get_sql_filenames_from_product(self):
+        'Can the system extract the SQL for a product?'
         setupDB = gs.recipe.createtables.setupdb.SetupDB()
         products = 'gs.option\n'
         r = setupDB.get_sql_filenames_from_products(products)
         self.assertEqual(1, len(r))
 
     def test_get_sql_filenames_from_multiple_products(self):
+        'Can the SQL be extracted for multiple products?'
         setupDB = gs.recipe.createtables.setupdb.SetupDB()
         products = 'gs.option\nProducts.GSAuditTrail\n'
         r = setupDB.get_sql_filenames_from_products(products)
         self.assertEqual(2, len(r))
+
+    def test_execute_psql_with_file(self):
+        'Can we construct a pipe to psql?'
+        with patch('gs.recipe.createtables.setupdb.Popen', autospec=True) as Po:
+            instance = Po.return_value
+            instance.stdout = MagicMock(name='stdout')
+            instance.stdout.read.return_value = 'East Kipling Road'
+            instance.wait.return_value = 0
+
+            setupDB = gs.recipe.createtables.setupdb.SetupDB()
+            r = setupDB.execute_psql_with_file('fake-user', 'db.example.com',
+                                    '5433', 'database', '/tmp/filename.sql')
+
+            self.assertEqual(0, r.returncode)
+            self.assertEqual('East Kipling Road', r.output)
