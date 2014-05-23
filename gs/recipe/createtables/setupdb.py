d09dbe13e9ecbaab742cd02931be60f1fd39d7ff
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-23 10:08:44 -0700

    Adding some docstrings.

--- gs/recipe/createtables/setupdb.py
+++ gs/recipe/createtables/setupdb.py
@@ -42,7 +42,7 @@ class SetupDB(object):
 :returns: The SQL files to load, in order.
 :rtype: A ``list`` of ``str``.'''
         retval = []
-        for moduleId in [p.strip() for p in products.split()]:
+        for moduleId in [p.strip() for p in products.split() if p.strip()]:
             module = import_module(moduleId)
             sqlDir = os.path.join(os.path.join(*module.__path__), 'sql')
             sqlFiles = glob(os.path.join(sqlDir, '*sql'))
@@ -52,6 +52,19 @@ class SetupDB(object):
         return retval
 
     def setup_database(self, user, host, port, database, products):
+        '''Setup the databases with the SQL files in the named products.
+
+:param str user: The PostgreSQL user.
+:param str host: The PostgreSQL host.
+:param str port: The PostgreSQL port.
+:param str database: The name of the PostgreSQL database to connect to.
+:param str products: The products to process, seperated by newline ``\n``
+                     characters.
+:returns: ``None``
+:raises SetupError: There was an issue processing a file.
+
+For each file that was successfully processed a ``.`` is displayed on the
+standard outout.'''
         e_sql = partial(self.execute_psql_with_file, user, host, port, database)
         for filename in self.get_sql_filenames_from_products(products):
             r = e_sql(filename=filename)
@@ -59,10 +72,27 @@ class SetupDB(object):
             if r.returncode == 0:
                 sys.stdout.write(m)
             else:
-                raise SetupError(m)
+                msg = 'Issue processing {0}\n{1}'.format(filename, m)
+                raise SetupError(msg)
 
     @staticmethod
     def execute_psql_with_file(user, host, port, database, filename):
+        '''Execute SQL in PostgreSQL
+
+:param str user: The PostgreSQL user.
+:param str host: The PostgreSQL host.
+:param str port: The PostgreSQL port.
+:param str database: The name of the PostgreSQL database to connect to.
+:param str filename: The name of the SQL file to execute.
+:returns: The return-code and output from :prog:`psql`.
+:rtype: :class:`OutputReturn`.
+
+The :meth:`execute_psql_with_file` method connects to the :prog:`psql`
+command-line programme with a pipe, passing the paramaters necessary to connect
+to the database. The output is read and returned along with the return-code
+from :prog:`psql`.
+
+.. seealso:: The :manpage:`psql(1)` manual page.'''
         # We pass the -w option to psql because we trust that the
         # gs_install_ubuntu.sh script has set up the PGPASSFILE environment
         # variable.
